5ad335c80aaf470894624ac2f4bf0d259cdcdeb8
Martin <martin@fau-stud-26-239.rrze.uni-erlangen.de>
2015-09-11 18:11:47 +0200

    first implementation from extractPacketsFromBytes

--- CTOpenSSLWrapper/OpenSSLWrapperTest/ViewController.m
+++ CTOpenSSLWrapper/OpenSSLWrapperTest/ViewController.m
@@ -25,7 +25,51 @@
 }
 
 - (void)extractPacketsFromBytes:(char *)bytes {
-
+    int tag = bytes[0];
+    int first_octet = bytes[1];
+    int second_octet = bytes[2];
+    int third_octet = bytes[3];
+    int fourth_octet = bytes[4];
+    int fiveth_octet = bytes[5];
+    int bodyLen = 0;
+    
+    if (tag == 0xC6) { //Tag 6 == Public Key Packet
+        NSLog(@"Tag == 6");
+        if (first_octet < 192) {
+            //Fill Body
+            bodyLen = first_octet;
+            NSLog(@"bodyLen = %d", bodyLen);
+            char body[bodyLen];
+            for (int i = 0; i < bodyLen; i++) {
+                body[i] = bytes[i+2];
+            }
+            [self extractPublicKeyFromBytes:body];
+            
+        }else if (first_octet < 234) {
+            //Fill Body
+            bodyLen = ((first_octet - 192) << 8) + (second_octet) + 192;
+            NSLog(@"bodyLen = %d", bodyLen);
+            char body[bodyLen];
+            for (int i = 0; i < bodyLen; i++) {
+                body[i] = bytes[i+3];
+            }
+            [self extractPublicKeyFromBytes:body];
+            
+        }else if (first_octet == 255) {
+            //Fill Body
+            bodyLen = (second_octet << 24) | (third_octet << 16) | (fourth_octet << 8)  | fiveth_octet;
+            NSLog(@"bodyLen = %d", bodyLen);
+            char body[bodyLen];
+            for (int i = 0; i < bodyLen; i++) {
+                body[i] = bytes[i+6];
+            }
+            [self extractPublicKeyFromBytes:body];
+            
+        }else {
+            //Exception
+            return;
+        }
+    }
 }
 
 - (void)extractPublicKeyFromBytes:(char *)bytes {
