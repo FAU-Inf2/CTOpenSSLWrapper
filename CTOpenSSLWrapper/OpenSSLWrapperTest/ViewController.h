35fc4a560d115a933db6af828367c56605567281
Jan Weiß <weiss19ja@gmail.com>
2015-09-11 15:02:38 +0200

    Added application target for testing the lib

---
+++ CTOpenSSLWrapper/OpenSSLWrapperTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenSSLWrapperTest
+//
+//  Created by Jan Weiß on 11.09.15.
+//  Copyright (c) 2015 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CTOpenSSLWrapper/OpenSSLWrapperTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  OpenSSLWrapperTest
+//
+//  Created by Jan Weiß on 11.09.15.
+//  Copyright (c) 2015 Home. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
