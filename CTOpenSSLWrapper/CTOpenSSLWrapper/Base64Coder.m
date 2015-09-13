5c0f5c4f1055fc886e288fa07d3bdacc4e9788f3
Jan Weiß <weiss19ja@gmail.com>
2015-09-13 20:16:21 +0200

    Added implementation for base64 encoding

--- CTOpenSSLWrapper/CTOpenSSLWrapper/Base64Coder.m
+++ CTOpenSSLWrapper/CTOpenSSLWrapper/Base64Coder.m
@@ -13,36 +13,46 @@
 
 @implementation Base64Coder
 
-+ (NSString *)encodeBase64String:(NSString *)encodedString {
-    /*
-     BIO *bio, *b64, *bio_out;
-     char inbuf[512];
-     int inlen;
-     
-     b64 = BIO_new(BIO_f_base64());
-     bio = BIO_new_fp(stdin, BIO_NOCLOSE);
-     bio_out = BIO_new_fp(stdout, BIO_NOCLOSE);
-     BIO_push(b64, bio);
-     while((inlen = BIO_read(b64, inbuf, 512)) > 0)
-     BIO_write(bio_out, inbuf, inlen);
-     
-     BIO_flush(bio_out);
-     BIO_free_all(b64);
-    */
-    BIO *bio, *b64, *bio_out;
-    char inbuf[strln([encodedString UTF8String])];
-    int inlen;
-    
-    b64 = BIO_new(BIO_f_base64());
-    bio = BIO_new_fp(stdin, BIO_NOCLOSE);
-    bio_out = BIO_new_fp(stdout, BIO_NOCLOSE);
-    BIO_push(b64, bio);
-    while ((inlen = BIO_read(b64, inbuf, strln([encodedString UTF8String]))) > 0) {
-        BIO_write(bio_out, inbuf, inlen);
-    }
-    
-    BIO_flush(bio_out);
-    BIO_free_all(b64);
++ (NSString *)getDecodedBase64StringFromString:(NSString *)string {
+    NSData *decodedData = [[NSData alloc] initWithBase64EncodedString:string options:0];
+    return [[NSString alloc] initWithData:decodedData encoding:NSUTF8StringEncoding];
 }
+
++ (NSString *)encodeBase64String:(NSString *)string {
+    NSData *plainData = [string dataUsingEncoding:NSUTF8StringEncoding];
+    return [plainData base64EncodedStringWithOptions:0];
+}
+
+//+ (NSString *)encodeBase64String:(NSString *)encodedString {
+//    /*
+//     BIO *bio, *b64, *bio_out;
+//     char inbuf[512];
+//     int inlen;
+//     
+//     b64 = BIO_new(BIO_f_base64());
+//     bio = BIO_new_fp(stdin, BIO_NOCLOSE);
+//     bio_out = BIO_new_fp(stdout, BIO_NOCLOSE);
+//     BIO_push(b64, bio);
+//     while((inlen = BIO_read(b64, inbuf, 512)) > 0)
+//     BIO_write(bio_out, inbuf, inlen);
+//     
+//     BIO_flush(bio_out);
+//     BIO_free_all(b64);
+//    */
+//    BIO *bio, *b64, *bio_out;
+////    char inbuf[strln([encodedString UTF8String])];
+//    int inlen;
+//    
+//    b64 = BIO_new(BIO_f_base64());
+//    bio = BIO_new_fp(stdin, BIO_NOCLOSE);
+//    bio_out = BIO_new_fp(stdout, BIO_NOCLOSE);
+//    BIO_push(b64, bio);
+////    while ((inlen = BIO_read(b64, inbuf, strln([encodedString UTF8String]))) > 0) {
+////        BIO_write(bio_out, inbuf, inlen);
+////    }
+//    
+//    BIO_flush(bio_out);
+//    BIO_free_all(b64);
+//}
 
 @end
