dd8f6f6a8e4a68bd7b0bd49267e0db38a19f64e3
Martin <martin.endrizzi@fau.de>
2015-09-13 12:49:24 +0200

    fixed possible issues in extractPublicKeyFromBytes

--- CTOpenSSLWrapper/OpenSSLWrapperTest/ViewController.m
+++ CTOpenSSLWrapper/OpenSSLWrapperTest/ViewController.m
@@ -21,6 +21,7 @@
 #import <openssl/err.h>
 #import <openssl/ssl.h>
 #import <openssl/md5.h>
+#import "PGPArmorHelper.h"
 
 @interface ViewController ()
 
@@ -31,7 +32,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    [self extractPublicKeyFromBytes:(char*) [@"H" UTF8String]];
+    [self extractPacketsFromBytes:[PGPArmorHelper removeArmorFromKeyFile:[[NSBundle mainBundle] URLForResource:@"publicTestKey" withExtension:@"asc"]]];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -47,43 +48,36 @@
     int fourth_octet = bytes[4];
     int fiveth_octet = bytes[5];
     int bodyLen = 0;
+    int offset = 0;
     
     if (tag == 0xC6) { //Tag 6 == Public Key Packet
         NSLog(@"Tag == 6");
         if (first_octet < 192) {
-            //Fill Body
             bodyLen = first_octet;
-            NSLog(@"bodyLen = %d", bodyLen);
-            char body[bodyLen];
-            for (int i = 0; i < bodyLen; i++) {
-                body[i] = bytes[i+2];
-            }
-            [self extractPublicKeyFromBytes:body];
+            offset = 2;
+            NSLog(@"bodyLen = %d, offset = %d", bodyLen, offset);
             
-        }else if (first_octet < 234) {
-            //Fill Body
+        }else if (first_octet < 224) {
             bodyLen = ((first_octet - 192) << 8) + (second_octet) + 192;
-            NSLog(@"bodyLen = %d", bodyLen);
-            char body[bodyLen];
-            for (int i = 0; i < bodyLen; i++) {
-                body[i] = bytes[i+3];
-            }
-            [self extractPublicKeyFromBytes:body];
+            offset = 3;
+            NSLog(@"bodyLen = %d, offset = %d", bodyLen, offset);
             
         }else if (first_octet == 255) {
-            //Fill Body
             bodyLen = (second_octet << 24) | (third_octet << 16) | (fourth_octet << 8)  | fiveth_octet;
-            NSLog(@"bodyLen = %d", bodyLen);
-            char body[bodyLen];
-            for (int i = 0; i < bodyLen; i++) {
-                body[i] = bytes[i+6];
-            }
-            [self extractPublicKeyFromBytes:body];
-            
+            offset = 6;
+            NSLog(@"bodyLen = %d, offset = %d", bodyLen, offset);
+
         }else {
             //Exception
             return;
         }
+        
+         //Fill Body
+        char body[bodyLen];
+        for (int i = 0; i < bodyLen; i++) {
+            body[i] = bytes[i+offset];
+        }
+        [self extractPublicKeyFromBytes:body];
     }
 }
 
@@ -109,14 +103,19 @@
     int p = 0;
     
     for (int i = 0; i < 2 && p < strlen(bmpi); i++) {
-        int len = bmpi[p] + bmpi[p+1];
-        NSLog(@"MPI %d len: %d", i, len);
-        BIGNUM* mpi = BN_new();
-        BN_set_word(mpi, 0);
-        for (int j = p+2; j < p+2+len; j++) {
-            BN_add_word(mpi, bmpi[j]);
-        }
-        NSLog(@"MPI %d value: %@", i, mpi);
+        double len = (bmpi[p] << 8) | bmpi[p+1];
+        int byteLen = ceil(len / 8);
+        NSLog(@"MPI %d len: %d", i, byteLen);
+        BIGNUM* payload = BN_new();
+        char mpi[strlen(bmpi)+2];
+        mpi[0] = 'a';
+        mpi[1] = 'a';
+        mpi[2] = '\0';
+        strncat(mpi, bmpi, strlen(bmpi));
+        mpi[0] = '\0';
+        mpi[1] = '\0';
+        BN_mpi2bn((const unsigned char*) mpi, byteLen, payload);
+        NSLog(@"MPI %d value: %@", i, payload);
         p += 2+len;
     }
     
